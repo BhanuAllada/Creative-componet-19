ea333f9822d13f4699974b7dd6f9ddaa6ee365c2
Bhanu Allada <bhanuallada95@gmail.com>
2019-06-08 13:08:42 -0500

    none

--- CNNPredict.m
+++
@@ -1,5 +0,0 @@
-Directory = uigetdir('C:\');
-imds = imageDatastore(Directory,'IncludeSubfolders',true);
-inputSize = [256 256 3];
-augimds = augmentedImageDatastore(inputSize(1:2),imds);
-YPred = predict(net,augimds)
